19bd38982973bf7443d6daac8b079858f4950264
Lucas Prazeres <olucascostadp@gmail.com>
2021-02-22 09:48:06 -0300

    refactor handle_file using pipe operator

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -6,10 +6,13 @@ defmodule Rocketpay.Numbers do
   end
 
   defp handle_file({:ok, result}) do
-    result = String.split(result, ",")
-    result = Enum.map(result, fn char -> String.to_integer(char) end)
-    result = Enum.sum(result)
+    result =
     result
+    |> String.split( ",")
+    |> Enum.map(fn char -> String.to_integer(char) end)
+    |> Enum.sum()
+
+    {:ok, %{ result: result }}
   end
   defp handle_file({:error, _reason}), do: {:error, "Invalid file."}
 end
