5131453478f50e3277a3dabc4cc75cb408aef4e6
Lucas Prazeres <olucascostadp@gmail.com>
2021-02-25 14:34:38 -0300

    fixed errors on deposit and withdraw modules

--- lib/rocketpay/accounts/transaction.ex
+++ lib/rocketpay/accounts/transaction.ex
@@ -18,8 +18,11 @@ defmodule Rocketpay.Accounts.Transaction do
 
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
-      {:error, _operation, reason, _changes} -> {:error, reason}
-      {:ok, result} -> IO.inspect(result)
+      {:error, _operation, reason, _changes} ->
+        {:error, reason}
+
+      {:ok, %{deposit: to_account, withdraw: from_account}} ->
+        {:ok, %{to_account: to_account, from_account: from_account}}
     end
   end
 end
